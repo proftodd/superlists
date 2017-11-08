c6607c1de3ec12b63c44098baed9acb78fd2b9b8
John Todd <John.Todd@ilwllc.com>
2017-11-08 15:35:36 -0500

    Model for list Items and associated migrations

--- lists/tests.py
+++ lists/tests.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 
 import re
 
+from lists.models import Item
 from lists.views import home_page
 
 csrf_regex = r'<input[^>]+csrfmiddle[^>]+>'
@@ -36,3 +37,22 @@ class HomePageTest(TestCase):
 		)
 		observed_html = re.sub(csrf_regex, '', response.content.decode())
 		self.assertEqual(observed_html, expected_html)
+
+class ItemModelTest(TestCase):
+
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
+		
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+		
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+		
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(second_saved_item.text, 'Item the second')
