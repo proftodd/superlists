b4ec809c967e579a5cf0336dc357edeff277b8f5
John Todd <John.Todd@ilwllc.com>
2017-11-08 15:16:01 -0500

    Adding multiple items gives numbered list
    Add a helper method and methodology to prevent
    StaleElementReferenceExceptions

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.keys import Keys
 import unittest
 
@@ -10,6 +11,19 @@ class NewVisitorTest(unittest.TestCase):
 
 	def tearDown(self):
 		self.browser.quit()
+		
+	def stale_aware_for_action(self, action):
+		while(True):
+			try:
+				action()
+				break
+			except StaleElementReferenceException:
+				continue
+		
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online to-do app. She goes
@@ -35,23 +49,31 @@ class NewVisitorTest(unittest.TestCase):
 		# When she hits enter, the page updates, and now the page lists
 		# "1. Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
+		def check_for_first_item_first_time():
+			self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.stale_aware_for_action(check_for_first_item_first_time)
 		
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows),
-			"New to-do item did not appear in table"
-		)
-
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
-		self.fail('Finish the test!')
+		def insert_second_item_to_inputbox():
+			inputbox = self.browser.find_element_by_id('id_new_item')
+			inputbox.send_keys('Use peacock feathers to make a fly')
+			inputbox.send_keys(Keys.ENTER)
+		self.stale_aware_for_action(insert_second_item_to_inputbox)
 
 		# The page updates again, and now shows both items on her list
+		def check_for_first_item_second_time():
+			self.check_for_row_in_list_table('1: Buy peacock feathers')
+		def check_for_second_item():
+			self.check_for_row_in_list_table('2. Use peacock feathers to make a fly')
+		
+		self.stale_aware_for_action(check_for_first_item_second_time)
+		self.stale_aware_for_action(check_for_second_item)
 
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
 		# explanatory text to that effect.
+		self.fail('Finish the test!')
 
 		# She visits that URL - her to-do list is still there.
 
--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,11 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
 
+import re
+
 from lists.views import home_page
+
+csrf_regex = r'<input[^>]+csrfmiddle[^>]+>'
 
 class HomePageTest(TestCase):
 
@@ -15,4 +19,20 @@ class HomePageTest(TestCase):
 		request = HttpRequest()
 		response = home_page(request)
 		expected_html = render_to_string('home.html')
-		self.assertEqual(response.content.decode(), expected_html)
+		observed_html = re.sub(csrf_regex, '', response.content.decode())
+		self.assertEqual(observed_html, expected_html)
+
+	def test_home_page_can_save_a_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+		
+		response = home_page(request)
+		
+		self.assertIn('A new list item', response.content.decode())
+		expected_html = render_to_string(
+			'home.html',
+			{'new_item_text': 'A new list item'}
+		)
+		observed_html = re.sub(csrf_regex, '', response.content.decode())
+		self.assertEqual(observed_html, expected_html)
