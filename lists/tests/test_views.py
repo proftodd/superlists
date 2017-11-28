b6dae04d29b988496a4837672235b8f0b9e70cd4
John Todd <john.todd@ilwllc.com>
2017-11-28 10:34:47 -0500

    finish duplicate checks

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -8,7 +8,10 @@ import re
 
 from unittest import skip
 
-from lists.forms import EMPTY_LIST_ERROR, ItemForm
+from lists.forms import (
+	DUPLICATE_ITEM_ERROR, EMPTY_LIST_ERROR,
+	ExistingListItemForm, ItemForm,
+)
 from lists.models import Item, List
 from lists.views import home_page
 
@@ -31,6 +34,18 @@ class ListViewTest(TestCase):
 		response = self.client.get('/lists/%d/' % (list_.id,))
 		self.assertTemplateUsed(response, 'list.html')
 
+	def test_passes_correct_list_to_template(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+		response = self.client.get('/lists/%d/' % (correct_list.id,))
+		self.assertEqual(response.context['list'], correct_list)
+	
+	def test_displays_item_form(self):
+		list_ = List.objects.create()
+		response = self.client.get('/lists/%d/' % (list_.id,))
+		self.assertIsInstance(response.context['form'], ExistingListItemForm)
+		self.assertContains(response, 'name="text"')
+
 	def test_displays_only_items_for_that_list(self):
 		correct_list = List.objects.create()
 		Item.objects.create(text = 'itemey1', list = correct_list)
@@ -46,12 +61,31 @@ class ListViewTest(TestCase):
 		self.assertNotContains(response, 'other list item 1')
 		self.assertNotContains(response, 'other list item 2')
 
-	def test_passes_correct_list_to_template(self):
+	def test_can_save_a_POST_request_to_an_existing_list(self):
 		other_list = List.objects.create()
 		correct_list = List.objects.create()
-		response = self.client.get('/lists/%d/' % (correct_list.id,))
-		self.assertEqual(response.context['list'], correct_list)
-	
+		
+		self.client.post(
+			'/lists/%d/' % (correct_list.id,),
+			data = {'text': 'A new item for an existing list'}
+		)
+		
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new item for an existing list')
+		self.assertEqual(new_item.list, correct_list)
+
+	def test_POST_redirects_to_list_view(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+		
+		response = self.client.post(
+			'/lists/%d/' % (correct_list.id,),
+			data = {'text': 'A new item for an existing list'}
+		)
+		
+		self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+
 	def post_invalid_input(self):
 		list_ = List.objects.create()
 		return self.client.post(
@@ -76,7 +110,6 @@ class ListViewTest(TestCase):
 		response = self.post_invalid_input()
 		self.assertContains(response, escape(EMPTY_LIST_ERROR))
 	
-	@skip
 	def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
 		list1 = List.objects.create()
 		item1 = Item.objects.create(list = list1, text = 'textey')
@@ -85,17 +118,11 @@ class ListViewTest(TestCase):
 			data = {'text': 'textey'}
 		)
 		
-		expected_error = escape("You've already got this in your list")
+		expected_error = escape(DUPLICATE_ITEM_ERROR)
 		self.assertContains(response, expected_error)
 		self.assertTemplateUsed(response, 'list.html')
 		self.assertEqual(Item.objects.all().count(), 1)
 	
-	def test_displays_item_form(self):
-		list_ = List.objects.create()
-		response = self.client.get('/lists/%d/' % (list_.id,))
-		self.assertIsInstance(response.context['form'], ItemForm)
-		self.assertContains(response, 'name="text"')
-
 class NewListTest(TestCase):
 
 	def test_saving_a_POST_request(self):
@@ -132,28 +159,3 @@ class NewListTest(TestCase):
 		self.client.post('/lists/new', data={'text': ''})
 		self.assertEqual(List.objects.count(), 0)
 		self.assertEqual(Item.objects.count(), 0)
-
-	def test_can_save_a_POST_request_to_an_existing_list(self):
-		other_list = List.objects.create()
-		correct_list = List.objects.create()
-		
-		self.client.post(
-			'/lists/%d/' % (correct_list.id,),
-			data = {'text': 'A new item for an existing list'}
-		)
-		
-		self.assertEqual(Item.objects.count(), 1)
-		new_item = Item.objects.first()
-		self.assertEqual(new_item.text, 'A new item for an existing list')
-		self.assertEqual(new_item.list, correct_list)
-
-	def test_POST_redirects_to_list_view(self):
-		other_list = List.objects.create()
-		correct_list = List.objects.create()
-		
-		response = self.client.post(
-			'/lists/%d/' % (correct_list.id,),
-			data = {'text': 'A new item for an existing list'}
-		)
-		
-		self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
